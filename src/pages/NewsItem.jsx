5fab3830fd3fa0957a70d9ee3386666566da83a1
Qaiser Saeed <qaiser.saeed25@gmail.com>
2025-08-27 12:55:02 +0500

    news Items

--- src/pages/NewsItem.jsx
+++ src/pages/NewsItem.jsx
@@ -14,7 +14,7 @@ const NewsItem = () => {
 
     axios
       .get(
-        `${apiUrl}/news/${id}?populate[News][populate][Feature_Image][populate]=*&populate[News][populate]=category`
+        `${apiUrl}/news/${id}?populate[News][populate][Feature_Image][populate]=*&populate[News][populate]=category&populate[News][populate][Banner_Image][populate]=*`
       )
       .then((res) => {
         setNewsItemData(res.data.data);
@@ -46,16 +46,16 @@ const NewsItem = () => {
         <div className="row banner-row mx-0">
           <div className="col-md-8 case_study-col-1 heading_part separate_col_1 blog_col-1 px-0 pb-5">
             <div className="inner-div">
-              <p className="font_20">ARTICLE</p>
+              <p className="font_20">{newsItemData?.News?.category?.name}</p>
               <h2
                 className="color_primary fw_600"
                 data-wow-duration="1s"
                 data-wow-delay="0.2s"
               >
                 <span className="text-dark fw_500">
-                  4 Reasons Why You Should Hire
+                  {newsItemData?.News?.Title}
                 </span>
-                <br /> a Consultant to Support Your Marketing Needs
+                <br /> {newsItemData?.News?.Tag_Line}
               </h2>
             </div>
           </div>
@@ -99,7 +99,7 @@ const NewsItem = () => {
       </div>
       {/* <!-----News-banner-section----> */}
 
-      <div className="container-fluid news_banner-img bg_cover">
+      <div className="container-fluid news_banner-img bg_cover" style={{backgroundImage: `url(${baseUrl}${newsItemData?.News?.Banner_Image?.url})`}}>
         <div className="container">
           <div className="row"></div>
         </div>
@@ -127,179 +127,9 @@ const NewsItem = () => {
           {/* <!-- Scrollable Content Right Column --> */}
           <div className="col-md-8 px-5 pt-5 right-sidebar pb-5">
             <section id="approach">
-              <h2>A Smart Business Strategy</h2>
-              <p>
-                In today’s fast-paced, information-overloaded world, the
-                importance of marketing and communications is greater than ever.
-                It’s no wonder that your marketing demands might be surpassing
-                your team’s capacity—whether your resources are maxed out or you
-                have a special project requiring focused attention, you need
-                help and you need it yesterday.   Here are four reasons why
-                hiring a consultant for your marketing needs is a smart business
-                strategy.
-              </p>
-              <ol className="ps-3">
-                <li>
-                  Fill Specialization Gaps We’re living in an increasingly
-                  specialized world, where marketing teams are composed of a
-                  range of specialists. You may already have marketing and
-                  communications professionals on staff but in today’s
-                  fast-paced world, gaps can quickly become evident. Utilizing a
-                  marketing consultant can bridge gaps and drive your marketing
-                  strategy forward with expert insights and flexible support.
-                </li>
-                <li>
-                  Beyond addressing skills gaps, there’s also the importance of
-                  industry knowledge and experience. By hiring a consultant with
-                  specific industry expertise, they will already understand the
-                  nuances of your business, while deep subject matter expertise
-                  will enable them to minimize the learning curve and quickly
-                  ramp up. As an expert in their field, a consultant hired from
-                  a consulting firm will be able to efficiently contribute to
-                  your team, blending in seamlessly and working to get critical
-                  projects completed.
-                </li>
-                <li>
-                  Provide Fresh Perspectives There’s a lot of talk these days
-                  about silos and breaking out of them. But, even if you’re able
-                  to knock down the interdepartmental walls within your own
-                  company, there’s still one silo that’s not only often hard to
-                  escape from, but whose existence is often hard to even
-                  recognize. That’s the silo of your company, and in some cases,
-                  your entire industry.
-                </li>
-                <li>
-                  Because consultants come from outside your company – but still
-                  have the expertise and knowledge to understand your industry –
-                  they are able to bring an outside perspective to your
-                  marketing and communications projects and challenges. An
-                  outside consultant with a deep background in product marketing
-                  across different categories may provide experience with
-                  activations and business trends unheard of within your own
-                  sector, but which promise to attract the attention of your
-                  audience, differentiating you right out of the starting gate.
-                </li>
-                <li>
-                  And, sometimes, the best thing that a fresh perspective can
-                  offer is reassurance you have the right strategies and tactics
-                  in place, as a consultant works with and helps guide your team
-                  across the finish line.
-                </li>
-                <li>
-                  Grow On-Demand The cost of hiring new employees can be
-                  substantial—a business faces expenses related to recruiting
-                  and onboarding, training, wages, benefits and payroll taxes.
-                  Even for smaller companies, these expenses can easily add up
-                  to tens of thousands of dollars for each new hire. And the
-                  impact of poor recruitment, high employee turnover, and an
-                  unsuccessful hire can lead to additional costs down the line.
-                  Studies show that the cost of replacing an employee can be
-                  1.5-2 times the employee’s annual salary.
-                </li>
-                <li>
-                  With the implications of cost-per-hire, an advantage of
-                  marketing and communications consultants is that they can be
-                  hired on a project-by-project or interim basis. A business
-                  owner can test the business strategy, the need, the skillset
-                  of a consultant, or hire for a temporary vacancy without
-                  having to go into all the costly risks of hiring and offering
-                  benefits and stock options.{" "}
-                </li>
-                <li>
-                  For instance, when a communications director for one national
-                  family welfare agency went out on maternity leave, a
-                  communications consultant was hired to continue driving
-                  multiple projects in her absence, including a social media
-                  audit, social media strategy development, and an SEO audit
-                  among other deliverables. The consultant had a brief period of
-                  onboarding prior to the client’s departure and continued the
-                  projects seamlessly to completion prior to the client’s
-                  return.
-                </li>
-                <li>
-                  Because consultants are only selected if they are at the top
-                  of their professions with a depth of knowledge and experience,
-                  you can be assured of rapid deployment and integration. There
-                  isn’t precious time spent on creating job vacancy ads, having
-                  them approved by HR and legal, then the long process of
-                  waiting for resumes to come in and reviewing them, followed by
-                  finally hiring and onboarding. Skilled and qualified marketing
-                  and communications consultants are the workforce equivalent of
-                  plug-and-play.
-                </li>
-                <li>
-                  Increase Growth, Not Friction Whether your company is a
-                  startup, a high growth organization, a Fortune 500 corporation
-                  or a regional non-profit, a common goal is often growth. But
-                  growth and business development isn’t always a straight line
-                  and can come with growing pains and associated costs. And it
-                  often means moving quickly and scaling to meet demands by
-                  adding additional talent and expertise.
-                </li>
-                <li>
-                  As we’ve mentioned, by leveraging a professional services firm
-                  to hire a consultant, companies can tap into a diverse pool of
-                  specialized skills and knowledge and experience, providing
-                  greater flexibility and efficiency. This approach reduces the
-                  necessity for a sizable in-house team, resulting in
-                  substantial cost savings and allowing businesses to scale and
-                  adapt their marketing strategies more effectively.
-                </li>
-                <li>
-                  Another benefit of a consultant is the ability to “try them on
-                  for size” before making a full-time hire.
-                </li>
-                <li>
-                  Not only can you scale quickly to meet growth demands, if the
-                  expertise is new to the business and you’re wondering if it
-                  requires creating a new role and adding headcount, a
-                  consultant allows you to prove the value and impact to the
-                  business, building a case to create a new full-time headcount.
-                  All of this while gaining immediate traction.
-                </li>
-                <li>
-                  Another advantage of project-based hiring compared to
-                  full-time employment is the positive flexibility and
-                  scalability it brings. By their very nature, project-based
-                  professionals are agile and adaptive. Their contribution can
-                  be both strategic and tactical based on your current and
-                  upcoming business priorities. As you approach an increased
-                  growth cycle, they can be brought in to help execute the extra
-                  work and then let go (without any concerns about exit
-                  compensation) enabling you to scale as needed for spurts of
-                  growth and expansion.
-                </li>
-                <li>
-                  Lastly, growth often means change, and we all know that not
-                  everyone is comfortable with change. That discomfort can lead
-                  to dissatisfaction and, ultimately, employee turnover. For
-                  example, it may become necessary to reorg internally to
-                  achieve growth targets and milestones.
-                </li>
-                <li>
-                  Bringing in outside perspective for marketing and
-                  communications can provide continuity in the event of this
-                  type of turmoil. Not only can it mean that your business is
-                  able to maintain its regular cadence of marketing efforts, but
-                  a marketing consultant can also help ensure your internal
-                  communications are effectively supporting the changes to the
-                  reorganization efforts. This drives adoption, increases
-                  employee engagement, and ensures staff turnover doesn’t become
-                  contagious.
-                </li>
-                <li>
-                  In today’s dynamic business environment, leveraging the
-                  expertise of marketing and communications consultants can be a
-                  game-changer. Whether it’s filling gaps in specialized skills,
-                  providing fresh perspectives, or enabling growth without
-                  friction, consultants offer flexible and efficient solutions
-                  tailored to your unique needs. At AMF Strategic Consultant
-                  Services, we have the experience and deep skilled workforce to
-                  help everyone from startups to Fortune 500 companies make
-                  their marcom efforts a success. Click here to start the
-                  conversation. 
-                </li>
-              </ol>
+              <div dangerouslySetInnerHTML={{ __html: newsItemData?.News?.Description_Title }}
+               ></div>
+              <div dangerouslySetInnerHTML={{ __html: newsItemData?.News?.Description }} ></div>
             </section>
           </div>
         </div>
