eb6563570062a0b0f8209949c2ada3b2750d11b2
Aneeza Anam Tariq <aneeza.anam.tariq@dynamologic.com>
2025-08-28 20:49:21 +0500

    updated talent exective page

--- src/pages/TalentExecutive.jsx
+++ src/pages/TalentExecutive.jsx
@@ -23,9 +23,9 @@ const TalentExecutive = () => {
   return (
     <>
       <style>{`
-.executive_banner{
-          margin-bottom: 80vh; 
-}
+// .executive_banner{
+//           margin-bottom: 80vh; 
+// }
             .split_parent{
       position: relative;
     }
@@ -38,7 +38,7 @@ const TalentExecutive = () => {
       background-color: #e9e9e9;
       position: sticky;
       top: 0;
-          margin-top: -50svh;
+          // margin-top: -50svh;
           display: flex;
           align-items: center;
           padding: 10px 5%
@@ -59,11 +59,15 @@ const TalentExecutive = () => {
     .img_3{
       margin-left: auto;
     }
+        .cc-text{
+        overflow: hidden
+        }
           .img_2{
           margin-left: auto
           }
     .cc-text-1{
-      margin-right: auto;
+      margin-left: auto;
+        margin-top: 0px;
     }
            .cc-text-3{
           margin-right: auto
@@ -71,9 +75,9 @@ const TalentExecutive = () => {
     .split_image-parent{
       height: 100%;
     }
-    .cc-text-1{
-          margin-top: -80svh;
-    }
+    // .cc-text-1{
+    //       margin-top: -20svh;
+    // }
    .hamburger i {
     color: #fff !important;
   }
@@ -94,7 +98,39 @@ const TalentExecutive = () => {
       }
           .executive_impact_section{
             background-attachment: fixed;
+        background-image: none !important;
           }
+        .impact_section_2{
+        background-image: url("images/impact_bg.png") !important;
+        position: sticky !important;
+        top: 0 !important
+        }
+        .orange_right_col{
+         background-image: url("images/orange-bg.jpg") !important;
+            background-size: cover;
+    background-position: center;
+        overflow: hidden
+        }
+        .impact_section h3.font_40 p, .impact_section h3.font_40{
+        font-size: 48pt !important;
+        }
+        .impact_section_2 .team_col::after{
+    left: auto !important;
+    right: -290px !important;
+        }
+        .empty_col{
+        position: relative
+        }
+        .empty_col::after{
+        position: absolute;
+    content: "";
+    height: 360px;
+    width: 360px;
+    background-color: #f2682ab5;
+    z-index: 1;
+    border-radius: 100%;
+    left: -175px;
+        }
 `}</style>
       <div className="container-fluid executive_banner">
         <div className="container">
@@ -109,10 +145,9 @@ const TalentExecutive = () => {
         </div>
       </div>
       {/* <!-- Impact-section --> */}
-
-      <div className="container-fluid impact_section  px-0 executive_impact_section position-relative">
-        {/*<div className="row mx-0">*/}
-        <div className=" cc-text split_col cc-text-1">
+  <div className="container-fluid impact_section impact_section_2 d-flex align-items-center px-0">
+        <div className="row mx-0">
+               <div className=" heading_col col-md-6 d-flex align-items-center px-5 team_col">
           <div className="px-5 content">
             <h3
               className="font_40 fw-semibold color_primary impact_section_heading wow animate__slideInLeft text-start"
@@ -129,17 +164,19 @@ const TalentExecutive = () => {
             ></div>
           </div>
         </div>
-        <div className=" cc-image split_col img_1 d-none"></div>
-        {/*     </div>
-      </div>*/}
-
-        {/* <!-- Impact-section-2 --> */}
-        {/* <div className="container-fluid impact_section impact_section_2 d-flex align-items-center px-0">
-        <div className="row mx-0">*/}
-        <div className="cc-image split_col img_2">
-          <img src="images/impact_bg.png" className="img-fluid"></img>
+        <div className="empty_col">
+          
         </div>
-        <div className="heading_col col-md-6 d-flex align-items-center px-5 team_col cc-text split_col img_2">
+        </div>
+    </div>
+
+    {/*end first section*/}
+      <div className="container-fluid impact_section  px-0 executive_impact_section position-relative">
+        {/*<div className="row mx-0">*/}
+  
+
+
+<div className=" cc-text split_col cc-text-1 ">
           <div className="px-5 content">
             <h3
               className="font_40 fw-semibold color_primary impact_section_heading wow animate__slideInLeft text-start"
@@ -156,6 +193,40 @@ const TalentExecutive = () => {
             ></div>
           </div>
         </div>
+
+
+        <div className=" cc-image split_col img_1 ">
+          <img src="images/left-impact-bg.jpg" className="img-fluid"></img>
+        </div>
+        {/*     </div>
+      </div>*/}
+
+        {/* <!-- Impact-section-2 --> */}
+        {/* <div className="container-fluid impact_section impact_section_2 d-flex align-items-center px-0">
+        <div className="row mx-0">*/}
+        <div className="col-md-6 px-5 d-flex align-items-center content_inner cc-image split_col img_3  orange_right_col">
+           <div className="px-5 content ">
+            <h3
+              className="font_40 fw-semibold text-dark wow animate__slideInLeft text-start"
+              data-wow-duration="1s"
+              data-wow-delay="0.3s"
+              dangerouslySetInnerHTML={{
+                __html: talentExectiveData?.Section5?.Column2_Heading || "",
+              }}
+            ></h3>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: talentExectiveData?.Section5?.Column2_Description || "",
+              }}
+            ></div>
+            <a
+              href={talentExectiveData?.Section5?.Column2_Button_URL}
+              className="btn_light mt-4 color_black"
+            >
+              {talentExectiveData?.Section5?.Column2_Button_Label}
+            </a>
+          </div>        </div>
+
         {/*   </div>
       </div>*/}
 
@@ -184,29 +255,9 @@ const TalentExecutive = () => {
         {/* <div className=" cc-image split_col img_2">
           <img src="images/impact_bg.png" className="img-fluid"></img>
         </div> */}
-        <div className="col-md-6 px-5 d-flex align-items-center content_inner cc-image split_col img_3 o_bg_primary">
-          <div className="px-5 content ">
-            <h3
-              className="font_40 fw-semibold text-dark wow animate__slideInLeft text-start"
-              data-wow-duration="1s"
-              data-wow-delay="0.3s"
-              dangerouslySetInnerHTML={{
-                __html: talentExectiveData?.Section5?.Column2_Heading || "",
-              }}
-            ></h3>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: talentExectiveData?.Section5?.Column2_Description || "",
-              }}
-            ></div>
-            <a
-              href={talentExectiveData?.Section5?.Column2_Button_URL}
-              className="btn_light mt-4 color_black"
-            >
-              {talentExectiveData?.Section5?.Column2_Button_Label}
-            </a>
-          </div>
-        </div>
+        {/*<div className="col-md-6 px-5 d-flex align-items-center content_inner cc-image split_col img_3 o_bg_primary">
+         
+        </div>*/}
       </div>
       {/*</div>*/}
 
